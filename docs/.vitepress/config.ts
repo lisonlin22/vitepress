96dfb6cfc845d388798d90fbcf40a981c4f73e58
LisonLIN <lisonlin22@gmail.com>
2022-07-22 15:23:38 +0800

    init

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,73 @@
+import { defineConfig } from "vitepress"
+export default defineConfig({
+    title: 'VitePress',
+    description: 'Just playing around.',
+    lastUpdated: true,
+    markdown: {
+        theme: 'material-palenight',
+        lineNumbers: true
+    },
+    themeConfig: {
+        logo: '/logo.svg',
+        siteTitle: 'VitePress',
+        // 搜索
+        algolia: {
+            appId: '8J64VVRP8K',
+            apiKey: 'a18e2f4cc5665f6602c5631fd868adfd',
+            indexName: 'vitepress'
+        },
+        // 导航栏
+        nav: nav(),
+        // 侧边栏
+        sidebar: sidebar(),
+        // 图标超链接
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+        ],
+        // 页面脚部
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: 'Copyright © 2019-present Evan You'
+        },
+        // 编辑链接
+        editLink: {
+            pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+            text: 'Edit this page on GitHub'
+        },
+
+    },
+
+})
+
+function nav() {
+    // 导航栏
+    return [
+        { text: 'Guide', link: '/guide/what-is-vitepress', activeMatch: '/guide/' },
+        {
+            text: "Version",
+            items: [
+                {
+                    text: 'Changelog',
+                    link: 'https://github.com/vuejs/vitepress/blob/main/CHANGELOG.md'
+                },
+                {
+                    text: 'Contributing',
+                    link: 'https://github.com/vuejs/vitepress/blob/main/.github/contributing.md'
+                },
+            ],
+        },
+    ]
+}
+function sidebar() {
+    // 侧边栏
+    return [
+        {
+            text: 'Introduction',
+            collapsible: true,
+            items: [
+                { text: 'What is VitePress?', link: '/guide/what-is-vitepress' },
+                { text: 'Getting Started', link: '/guide/getting-started' },
+            ]
+        }
+    ]
+}
---
+++ docs/index.md
@@ -0,0 +1,33 @@
+---
+layout: home
+
+title: VitePress
+titleTemplate: Vite & Vue Powered Static Site Generator
+heroImage: https://cn.vitejs.dev/logo-with-shadow.png
+hero:
+  name: VitePress
+  text: Vite & Vue Powered Static Site Generator
+  tagline: Simple, powerful, and performant. Meet the modern SSG framework you've always wanted.
+  image: /logo-with-shadow.png
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /guide/getting-started
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/vuejs/vitepress
+
+features:
+  - icon: 😄
+    title: "Vite: The DX that can't be beat"
+    details: Feel the speed of Vite. Instant server start and lightning fast HMR that stays fast regardless of the app size.
+  - icon: 📦
+    title: Designed to be simplicity first
+    details: With Markdown-centered content, it's built to help you focus on writing and deployed with minimum configuration.
+  - icon: 🛠️
+    title: Power of Vue meets Markdown
+    details: Enhance your content with all the features of Vue in Markdown, while being able to customize your site with Vue.
+  - icon: ⚡️
+    title: Fully static yet still dynamic
+    details: Go wild with true SSG + SPA architecture. Static on page load, but engage users with 100% interactivity from there.
+---
